ba9e721a9854cc5da1c87f85c9f04518205e4f07
Kevin <kioo@hotmail.fr>
2017-12-04 15:59:45 +0100

    move configuration of eslint in specific package

---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = {
+  'env': {
+    'es6': true,
+    'node': true,
+    'browser': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'eslint-config-ionic'
+  ],
+  'rules': {
+    'comma-style': ['error'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': ['error', {
+      'before': false,
+      'after': true
+    }],
+    'indent': ['error', 4, {
+      'SwitchCase': 1
+    }],
+    'newline-before-return': ['error'],
+    'newline-per-chained-call': ['error'],
+    'no-process-env': ['off'],
+    'no-undefined': ['error'],
+    'no-unused-vars': ['warn'],
+    'quotes': ['error', 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true
+    }],
+    'quote-props': ['error', 'always'],
+    'valid-jsdoc': ['off', {
+      'prefer': {
+        'return': 'returns'
+      },
+      'requireParamDescription': false,
+      'requireReturnDescription': false
+    }],
+    'space-before-function-paren': ['error', {
+      'anonymous': 'always',
+      'named': 'never'
+    }]
+  }
+};
