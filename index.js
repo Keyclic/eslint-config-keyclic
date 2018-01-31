faf64137e6a6f89b83d84f61193313d285f64488
Kevin <kioo@hotmail.fr>
2018-01-31 18:32:10 +0100

    Simplify and remove eslint Ionic config

--- index.js
+++ index.js
@@ -1,44 +1,23 @@
 module.exports = {
-  'env': {
-    'es6': true,
-    'node': true,
-    'browser': true
-  },
-  'extends': [
-    'eslint:recommended',
-    'ionic',
-    'standard'
-  ],
-  'rules': {
-    'comma-style': ['error'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'comma-spacing': ['error', {
-      'before': false,
-      'after': true
-    }],
-    'indent': ['error', 4, {
-      'SwitchCase': 1
-    }],
-    'newline-before-return': ['error'],
-    'newline-per-chained-call': ['error'],
-    'no-process-env': ['off'],
-    'no-undefined': ['error'],
-    'no-unused-vars': ['warn'],
-    'quotes': ['error', 'single', {
-      'avoidEscape': true,
-      'allowTemplateLiterals': true
-    }],
-    'quote-props': ['error', 'always'],
-    'valid-jsdoc': ['off', {
-      'prefer': {
-        'return': 'returns'
-      },
-      'requireParamDescription': false,
-      'requireReturnDescription': false
-    }],
-    'space-before-function-paren': ['error', {
-      'anonymous': 'always',
-      'named': 'never'
-    }]
-  }
+    'env': {
+        'es6': true,
+        'node': true,
+        'browser': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'standard'
+    ],
+    'rules': {
+        'comma-dangle': ['error', 'always-multiline'],
+        'sort-imports': 'error',
+        'indent': ['error', 4, {
+            'SwitchCase': 1
+        }],
+        'space-before-function-paren': ['error', {
+            'anonymous': 'always',
+            'named': 'never'
+        }],
+        'semi': ['error', 'always']
+    }
 };
